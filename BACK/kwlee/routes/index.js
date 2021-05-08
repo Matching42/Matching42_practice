287f2d4e698b96e3b2c056dda1496e9deb2cc8b3
Lks9172 <ekdan9172@naver.com>
2021-05-08 22:33:42 +0900

    feat : middleware 구현
    - console에 '정상접속중'출력하는 미들웨어 구현
    - '/kwlee', '/hello' 라우팅에 '정상접속중' 미들웨어 적용

--- BACK/kwlee/routes/index.js
+++ BACK/kwlee/routes/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const middle = require("../middleware");
 
 router.get("/", (req, res) => {
   res.send("server start");
 });
 
-router.get("/kwlee", (req, res) => {
+router.get("/kwlee", middle, (req, res) => {
   res.send("matching42");
 });
 
-router.get("/hello", (req, res) => {
+router.get("/hello",middle, (req, res) => {
 	res.render("../views/hello.html");
   });
 
